7c4d145e42cf66e94822e7e95495543223810675
Paul-Andrei Petan <pap36@bath.ac.uk>
2023-09-25 12:55:38 +0100

    Migrated to version catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,13 +16,13 @@ android {
 }
 
 dependencies {
+    // Compose BOM
+    implementation(platform(libs.androidx.compose.bom))
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.7.2")
-    implementation(platform("androidx.compose:compose-bom:2023.09.01"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.androidx.activity.compose)
+
+    // Extended icons
+    implementation(libs.androidx.compose.material.iconsExtended)
 }
